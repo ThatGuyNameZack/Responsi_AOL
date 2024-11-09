cec2618bd10787f7491cd1480f47dad89c3adee5
Rafi <50817438+ThatGuyNameZack@users.noreply.github.com>
2024-11-09 13:03:25 +0700

    Add files via upload

---
+++ no 2/FindError.java
@@ -0,0 +1,12 @@
+//orignally there was 3e elemnt so it wont callout the 4th so basically just add 1 more element
+//thats it basically
+public class FindError{
+    public static void main(String[] args){
+        String message = "Hello World";
+        System.out.println(message);
+
+    int[] numbers = {1,2,3,4};
+        System.out.println("we want the fourth number" +numbers[3]);
+    }
+}
+
---
+++ no1/FindError.java
@@ -0,0 +1,11 @@
+public class FindError { 
+    public static void main (String[]args){
+        int number = 10;
+        System.out.println("the number is:" + number);
+        
+        int result = number / 0;
+        System.out.println("The result is" + result);
+
+    }
+
+}
